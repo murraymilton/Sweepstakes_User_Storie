8a05e10044bf052dbc34adae267bac62e3877b31
murraymilton <murray.l.milton@outlook.com>
2021-06-23 20:27:18 +0200

    Added an instance of the user-interface class to the menu method to allow the user to interact with menu options. Corrected the syntax in the marketing class for preparation to finalizing the facade interface for users.

--- marketing_firm.py
+++ marketing_firm.py
@@ -1,4 +1,4 @@
-import user_interface
+from user_interface import UserInterface
 from sweepstakes import Sweepstakes
 
 
@@ -8,19 +8,26 @@ class Marketing_Firm:
         self.marketing_firm_name = name
         self.sweepstakes_inventory = []
 
-    def create_sweepstakes(self):
-        sweepstakes = Sweepstakes()
-        self.sweepstakes_inventory.append(user_interface.get_user_input_string())
-
     def select_sweepstakes(self):
         selected_sweepstakes = user_interface.get_user_input_number()
         return selected_sweepstakes
 
-    def change_marketing_firm_name(self):
-        self.marketing_firm_name = user_interface.get_user_input_string()
+    def create_sweepstakes(self):
+        sweepstakes = Sweepstakes()
+        self.sweepstakes_inventory.append(user_interface.get_user_input_string())
+
+
+    @staticmethod
+    def change_marketing_firm_name():
+        change_marketing_firm = user_interface.get_user_input_string()
+        select_new_marketing_firm = change_marketing_firm
+        return select_new_marketing_firm
+
 
 
     def menu(self):
+        user_selection = UserInterface.get_user_input_number("Please enter your selection")
+        pass
         self.create_sweepstakes()
         self.change_marketing_firm_name()
         self.select_sweepstakes()
--- sweepstakes.py
+++ sweepstakes.py
@@ -1,12 +1,14 @@
 import random
 from contestant import Contestant
+from user_interface import UserInterface
 
 
 class Sweepstakes:
 
     def __init__(self, name):
         self.sweepstakes_name = name
-        self.contestants = []
+        self.contestants = {}
+        contestant = Contestant()
 
     def register_contestant(self, contestant):
         self.contestants.update(self.get_registration_number(contestant))
@@ -14,15 +16,19 @@ class Sweepstakes:
     def get_registration_number(self, contestant):
         contestant.contestant_registration_number = 0000
         for contestant in self.contestants:
-            self.contestant_registration_number += 1
+            contestant.contestant_registration_number += 1
 
     def pick_winner(self):
         winner_announced = random.randint(self.contestants)
+        Contestant.notify_winner()
+        return winner_announced
 
     def view_contestants(self):
-        return self.contestants
+        return UserInterface.display_contestant_info()
 
     def menu(self):
+        user_selection = UserInterface.get_user_input_number("Please make a selection:")
         self.register_contestant()
         self.view_contestants()
         self.announce_winner()
+        pass
