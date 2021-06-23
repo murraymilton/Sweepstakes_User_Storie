3c1657b63d9678265d6f628ef5a0f87797a0ecf1
murraymilton <murray.l.milton@outlook.com>
2021-06-23 22:13:46 +0200

    Added static methods to the user interface of object instances

--- user_interface.py
+++ user_interface.py
@@ -3,19 +3,36 @@ class UserInterface:
 
     @staticmethod
     def display_message(message):
-        print(f"Welcome")
         print(message)
 
 
     @staticmethod
     def get_user_input_string(prompt):
-
         user_input = input(prompt)
         return user_input
 
     @staticmethod
     def get_user_input_number(prompt):
+        response = int(input(prompt))
+        return response
 
-        user_input = int(input(prompt))
-        return user_input
-        pass
+    @staticmethod
+    def display_contestant_info(contestant):
+        print(contestant.first_name)
+        print(contestant.last_name)
+        print(contestant.email_address)
+        print(contestant.registration_number)
+
+    @staticmethod
+    def display_sweepstakes_info(sweepstakes):
+        print(sweepstakes.sweepstakes_name)
+        print(f'{len(sweepstakes.contestants)} contestants')
+
+    @staticmethod
+    def display_marketing_firm_menu_options(marketing_firm_name):
+        return marketing_firm_name
+
+
+    @staticmethod
+    def display_sweepstakes_menu_options(sweepstakes_name):
+        return sweepstakes_name
